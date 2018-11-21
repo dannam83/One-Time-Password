a57eb364306896e4bd32a8149a98a737350d0fff
Daniel Nam <dannam83@gmail.com>
2018-11-21 07:28:44 -0500

    setup anyonymous user creation

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,18 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-  res.send(req.body);
+  // verify the user provided a phone number. 422 for user error.
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  // format the phone number
+  // regex is selecting anything not a number. actual regex between brackets.
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // create new user with phone number with phone as unique id
+  // this is asynchronous so use promises
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
 }
